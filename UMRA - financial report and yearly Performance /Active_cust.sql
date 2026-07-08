01a84b37f256351ef73390f56556fe66ef2cc9fd
velayuthamFlow <velayutham.m@in.flowglobal.net>
2026-07-08 22:01:22 +0530

    code changes

--- UMRA - financial report and yearly Performance /Active_cust.sql
+++ UMRA - financial report and yearly Performance /Active_cust.sql
@@ -1,6 +1,5 @@
-
 SET @country_code = 'RWA';
-SET @month = '202512';
+SET @month = '202606';
 
 SET @pre_month = DATE_FORMAT(
     DATE_SUB(STR_TO_DATE(CONCAT(@month, '01'), '%Y%m%d'), INTERVAL 1 MONTH),
@@ -8,6 +7,7 @@ SET @pre_month = DATE_FORMAT(
 );
 
 SET @start_date     = CONCAT(@month, '01 00:00:00');
+SET @end_date       = LAST_DAY(CONCAT(@month, '01'));
 SET @last_day       = CONCAT(LAST_DAY(CONCAT(@month, '01')), ' 23:59:59');
 
 SET @realization_date = IFNULL(
@@ -31,7 +31,7 @@ SET @pre_realization_date = (
     LIMIT 1
 );
 
-SET @loan_purpose = 'growth_financing'
+-- SET @loan_purpose = 'growth_financing'
 
 
 WITH latest_record_audits AS (
@@ -47,24 +47,34 @@ WITH latest_record_audits AS (
 )
 
 SELECT
-    l.loan_purpose,
-    COUNT(DISTINCT b.cust_id) AS active_cust_count
+    count(DISTINCT b.cust_id) as total_cust_id,
+   COUNT(DISTINCT IF(p.gender != 'female' or p.gender is null , b.cust_id, NULL)) AS male,
+    COUNT(DISTINCT IF(p.gender = 'female', b.cust_id, NULL)) AS female,
+    COUNT(DISTINCT IF( TIMESTAMPDIFF(YEAR, p.dob, @end_date) <= 30, b.cust_id, NULL)) AS youth_count,
+    COUNT(DISTINCT IF( TIMESTAMPDIFF(YEAR, p.dob, @end_date) > 30 or p.dob is null, b.cust_id, NULL)) AS old_cust_count,
+    COUNT(DISTINCT CASE WHEN  b.country_code = 'UGA' AND ( a.field_2 != 'kampala') THEN b.cust_id
+                        WHEN  b.country_code = 'RWA' AND ( a.field_1 != 'Kigali' ) THEN b.cust_id END ) AS rural_count,
+    COUNT(DISTINCT CASE WHEN  b.country_code = 'UGA' AND ( a.field_2 = 'kampala' or a.field_2 is null) THEN b.cust_id
+                        WHEN  b.country_code = 'RWA' AND ( a.field_1 = 'Kigali' or a.field_1 is null) THEN b.cust_id END ) AS urben_count
 FROM borrowers b
 JOIN loans l     ON b.cust_id = l.cust_id
 JOIN loan_txns t ON l.loan_doc_id = t.loan_doc_id
+LEFT Join persons p  ON b.owner_person_id = p.id
+LEFT JOIN address_info a ON a.id = b.owner_address_id
 WHERE b.country_code = @country_code
   AND t.txn_type IN ('disbursal','af_disbursal')
   AND t.txn_date BETWEEN DATE_SUB(@last_day, INTERVAL 30 DAY) AND @last_day
   AND l.product_id NOT IN (43,75,300)
   AND l.status NOT IN ('voided','hold','pending_disbursal','pending_mnl_dsbrsl')
-  AND l.loan_purpose in (@loan_purpose)
+  -- AND l.loan_purpose in (@loan_purpose)
   AND NOT EXISTS (
         SELECT 1
         FROM latest_record_audits ra
         WHERE ra.record_code = b.cust_id
           AND ra.status = 'disabled'
   )
-GROUP BY l.loan_purpose;
+-- GROUP BY l.loan_purpose
+  ;
 
 
 
